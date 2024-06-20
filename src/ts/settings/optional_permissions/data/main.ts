ca41c362b2af0f17c0f3eabe0cb8b14fbaa23c97
loftyshaky <loftyshaky@gmail.com>
2024-06-20 22:48:23 +1000

    fix: check if contains permission

--- src/ts/settings/optional_permissions/data/main.ts
+++ src/ts/settings/optional_permissions/data/main.ts
@@ -12,20 +12,27 @@ export class Main {
     // eslint-disable-next-line no-useless-constructor, no-empty-function
     private constructor() {}
 
+    private optional_permission_checkbox_dict = {
+        filter_lists: { permissions: ['tabs'], origins: [] },
+    };
+
     public set_permission = ({ input }: { input: i_inputs.Input }): Promise<boolean> =>
         err_async(async () => {
             const is_tabs_permission_input: boolean = ['url_whitelist', 'url_blacklist'].includes(
                 input.name,
             );
+            const contains_permission: boolean = await we.permissions.contains(
+                this.optional_permission_checkbox_dict.filter_lists,
+            );
 
-            return is_tabs_permission_input
-                ? d_optional_permissions.Main.i().set_permission({
-                      name: 'filter_lists',
-                      optional_permission_checkbox_dict: {
-                          filter_lists: { permissions: ['tabs'], origins: [] },
-                      },
-                      set_checkbox_val: false,
-                  })
-                : true;
+            if (is_tabs_permission_input && !contains_permission) {
+                return d_optional_permissions.Main.i().set_permission({
+                    name: 'filter_lists',
+                    optional_permission_checkbox_dict: this.optional_permission_checkbox_dict,
+                    set_checkbox_val: false,
+                });
+            }
+
+            return true;
         }, 'cot_1086');
 }
