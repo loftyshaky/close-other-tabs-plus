01a83284946bc584b96404c6e0ad9dc6503a740c
loftyshaky <loftyshaky@gmail.com>
2024-10-05 01:13:58 +1000

    fix: remove unneeded interface

--- src/ts/shared_clean/data/interfaces/session.ts
+++
@@ -1,9 +0,0 @@
-import { i_actions, i_data } from 'shared_clean/internal';
-
-export interface Session {
-    settings: { [index: string]: i_data.Prefs | i_actions.Action };
-    current_action: i_actions.Action;
-    main_action: i_actions.Action;
-    actions: i_actions.Action[];
-    updating_settings: boolean;
-}
